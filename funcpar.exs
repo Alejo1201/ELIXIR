74bdd0bf71f71fae3679ecbd219b3ceb02394536
Alejo1201 <equinterogiraldo12@gmail.com>
2022-09-27 22:54:47 -0500

    Nuevas funciones

--- funcpar.exs
+++ funcpar.exs
@@ -4,3 +4,42 @@ defmodule Calculon do
     func.(5,3)
   end
 end
+
+IO.puts Calculon.secret_add()
+
+defmodule Secrets do
+  def secret_add(secret) do
+    # Please implement the secret_add/1 function
+    fn adder -> adder + secret end
+  end
+  def secret_subtract(secret) do
+    # Please implement the secret_subtract/1 function
+    fn subs -> subs - secret end
+  end
+
+  def secret_multiply(secret) do
+    # Please implement the secret_multiply/1 function
+    fn multi -> multi * secret end
+  end
+
+  def secret_divide(secret) do
+    # Please implement the secret_divide/1 function
+    fn divi -> div(divi, secret ) end
+  end
+
+  def secret_and(secret) do
+    # Please implement the secret_and/1 function
+      fn y -> Bitwise.band(y, secret) end
+  end
+
+  def secret_xor(secret) do
+    # Please implement the secret_xor/1 function
+      fn xor -> Bitwise.bxor(xor, secret) end
+
+  end
+
+  def secret_combine(secret_function1, secret_function2) do
+    # Please implement the secret_combine/2 function
+    fn combi -> (secret_function2.(secret_function1.(combi))) end
+  end
+end
